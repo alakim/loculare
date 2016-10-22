ed32a671d3706e312b3772c602cd78871587939e
alakim <alakm@rambler.ru>
2016-10-22 12:32:35 +0300

    Отрисовка произвольной формы

--- js/loculare.js
+++ js/loculare.js
@@ -1,30 +1,56 @@
 var Loculare = (function($, $H, $D){
-
-	function division(c, s){
-		var r0 = c.attr("r");
-		var cd = c.clone();
-		cd.drag();
-		cd.click(function(ev){
-			if(ev.ctrlKey){
-				division(cd,s);
-			}
-		});
-		c.animate({
-			r:r0/2,
-			cx:+c.attr("cx")-r0/2
-		}, Loculare.settings.divtime);
-		cd.animate({
-			r:r0/2,
-			cx:+c.attr("cx")+r0/2
-		}, Loculare.settings.divtime);
-	}
 	
 	function spawn(snap, pos){
-		var protLoc = snap.circle(pos.x, pos.y, 50).attr({
+
+		function division(c, s){
+			var r0 = c.attr("r");
+			var cd = c.clone();
+			cd.drag();
+			cd.click(function(ev){
+				if(ev.ctrlKey){
+					division(cd,s);
+				}
+			});
+			c.animate({
+				r:r0/2,
+				cx:+c.attr("cx")-r0/2
+			}, Loculare.settings.divtime);
+			cd.animate({
+				r:r0/2,
+				cx:+c.attr("cx")+r0/2
+			}, Loculare.settings.divtime);
+		}
+
+		var size = 50;
+		var path = [
+			'M', pos.x, ',', pos.y,
+			'L', pos.x+size, ',', pos.y,
+			'L', pos.x+size, ',', pos.y+size,
+			'L', pos.x+size/1.5, ',', pos.y+size/2,
+			'L', pos.x+size/3, ',', pos.y+size/.5,
+			'L', pos.x, ',', pos.y+size, 
+			'Z'
+		].join('');
+		
+		var protLoc;
+		
+		switch(Loculare.settings.view){
+			case 'path': protLoc = snap.path(path);
+				break;
+			case 'rect': protLoc = snap.rect(pos.x, pos.y, 50, 50);
+				break;
+			case 'circle':
+			default:
+				protLoc = snap.circle(pos.x, pos.y, 50);
+				break;
+		}
+		
+		protLoc.attr({
 			fill:"#f00",
 			stroke:"#004",
 			strokeWidth:5
 		});
+		
 		protLoc.drag();
 		protLoc.click(function(ev){
 			// console.log(ev);
@@ -38,7 +64,8 @@ var Loculare = (function($, $H, $D){
 	
 	return {
 		settings:{
-			divtime: 1500   // время анимации деления
+			divtime: 1500,   // время анимации деления
+			view: 'circle' // вид отображения (circle, rect, path)
 		},
 		spawn: spawn
 	};
