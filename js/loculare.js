7efee135a59f14c3e50a3d0c15dfaca1b2b24d42
alakim <alakm@rambler.ru>
2016-11-26 22:03:16 +0300

    Цикл анимации

--- js/loculare.js
+++ js/loculare.js
@@ -18,11 +18,20 @@ var Loculare = (function($, $H, $D, $V){
 			cx:+c.attr("cx")+r0/2
 		}, Loculare.settings.divtime);
 	}
-	
-	function collision(x1,y1,r1,x2,y2,r2){
-        	return $V.length2(x1, y1, x2, y2) <=(r1+r2)(r1+r2);
+
+	var requestAnimFrame = window.requestAnimationFrame ||
+		window.webkitRequestAnimationFrame ||
+		window.mozRequestAnimationFrame ||
+		window.oRequestAnimationFrame ||
+		window.msRequestAnimationFrame ||
+		function (callback) {setTimeout(callback, 16);};
+
+
+
+	function collision(x1, y1, r1, x2, y2, r2){
+		return $V.length2(x1, y1, x2, y2) <=(r1 + r2)(r1 + r2);
 	}
-	
+
 	function spawn(snap, pos){
 		var protLoc = snap.circle(pos.x, pos.y, 50).attr({
 			fill:"#f00",
@@ -37,20 +46,54 @@ var Loculare = (function($, $H, $D, $V){
 				division(protLoc, snap);
 			}
 		});
-		
+
+		skeleton(snap, pos);
+
+
+		var testPoint = snap.circle(50, 50, 10).attr({fill:'#00ff00', stroke:'#000022'});
+		var velocity = .1;
+		function animationStep(frameNumber, t0){
+			var curT = (new Date).getTime();
+			var dT = curT - t0;
+			var curVel = velocity + (frameNumber-10)*(frameNumber-10)/100;
+			var dX = dT * curVel;
+			var x0 = +testPoint.attr('cx');
+			testPoint.attr({cx:x0 + dX});
+			requestAnimFrame(function(){
+				animationStep(frameNumber+1, curT);
+			});
+		}
+		animationStep(0, (new Date).getTime());
+
 	}
-	
-	function skeleton(){ // скелет амебы
+
+	function skeleton(snap, pos){ // скелет амебы
 		var nodeCountRange = {min:4, max:6}; // ограничение на количество узлов
-		var nodeCount = Math.random()*(nodeCountRange.max-nodeCountRange.min) + nodeCountRange.min;
-		var alphaRad = Math.PI*2/nodeCount; // угол между узлами
-		var r0 = 100;
+		var nodeCount = Math.round(
+			Math.random()*
+			(nodeCountRange.max-nodeCountRange.min) + nodeCountRange.min
+		);
+
+		var alphaRad = (Math.PI*2.0)/nodeCount; // угол между узлами
+
+		var r0 = 80;
 		var nodes = []; // массив координат узлов
 		for(var i=0; i<nodeCount; i++){
 			nodes.push($V.polar(r0, alphaRad*i));
 		}
+
+		var ndCenter = snap.circle(pos.x, pos.y, 8);
+		ndCenter.drag();
+		$D.each(nodes, function(nd){
+			var x = nd[0]+pos.x,
+				y = nd[1]+pos.y;
+			var ndView = snap.circle(x, y, 8);
+			var bound = snap.line(pos.x, pos.y, x, y).attr({stroke:'#0000ff', 'stroke-width':3});
+			ndView.drag();
+		});
+
 	}
-	
+
 	return {
 		settings:{
 			divtime: 1500   // время анимации деления
@@ -58,4 +101,3 @@ var Loculare = (function($, $H, $D, $V){
 		spawn: spawn
 	};
 })(jQuery, Html.version('4.1.0'), JDB.version('3.0.1'), Vector2);
-
--- js/vector2.js
+++ js/vector2.js
@@ -5,28 +5,29 @@ var Vector2 = (function(){
 			v1[1]+y2
 		];
 	}
-	
+
 	function mul (v, a){
 		return [
 			v[0]*a,
 			v[1]*a
 		];
 	}
-	
+
 	function length2 (x1, y1, x2, y2){
 		return (x1-x2)*(x1-x2)+(y1-y2)*(y1-y2);
 	}
-	
+
 	function polar(r, alphaRad){
 		return [
 			r * Math.cos(alphaRad),
 			r * Math.sin(alphaRad)
 		];
 	}
-	
+
 	return {
 		add:add,
 		mul:mul,
-		length2:length2
+		length2:length2,
+		polar:polar
 	};
 })();
